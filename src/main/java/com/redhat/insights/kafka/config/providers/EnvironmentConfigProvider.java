c44b8eca7daf7b8df496fe0c9384aa118ea34b0e
ckyrouac <ckyrouac@redhat.com>
2021-08-18 13:48:50 -0400

    Add EnvironmentConfigProvider

---
+++ src/main/java/com/redhat/insights/kafka/config/providers/EnvironmentConfigProvider.java
@@ -0,0 +1,47 @@
+package com.redhat.insights.kafka.config.providers;
+
+import org.apache.kafka.common.config.ConfigData;
+import org.apache.kafka.common.config.provider.ConfigProvider;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * {@link ConfigProvider} implementation that reads values from environment variables.
+ *
+ * Example:
+ *
+ * "database.password": "${env:DB_PASSWORD}",
+ */
+public class EnvironmentConfigProvider implements ConfigProvider {
+
+    @Override
+    public void configure(Map<String, ?> cfg) {
+    }
+
+    @Override
+    public void close() throws IOException {
+    }
+
+    @Override
+    public ConfigData get(String path) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ConfigData get(String path, Set<String> keys) {
+        if (path == null || path.length() != 0) {
+            throw new IllegalArgumentException("Only basic syntax (e.g. ${env:VARIABLE_NAME}) is supported");
+        }
+
+        return new ConfigData(keys.stream().collect(Collectors.toMap(key -> key, key -> {
+            String value = System.getenv(key);
+            if (value == null) {
+                throw new EnvironmentVariableMissingException("Environment variable " + key + " not found");
+            }
+            return value;
+        })));
+    }
+}
---
+++ src/test/java/com/redhat/insights/kafka/config/providers/EnvironmentConfigProviderTest.java
@@ -0,0 +1,45 @@
+package com.redhat.insights.kafka.config.providers;
+
+import org.apache.kafka.common.config.ConfigData;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junitpioneer.jupiter.SetEnvironmentVariable;
+
+import java.io.IOException;
+import java.util.Collections;
+
+public class EnvironmentConfigProviderTest {
+
+    private EnvironmentConfigProvider instance;
+
+    @BeforeEach
+    public void init() {
+        instance = new EnvironmentConfigProvider();
+        instance.configure(Collections.emptyMap());
+    }
+
+    @AfterEach
+    public void close() throws IOException {
+        instance.close();
+    }
+
+    @Test
+    @SetEnvironmentVariable(key = "DATABASE_HOSTNAME", value = "a.test.db")
+    public void testReadingEnvironmentVariable() {
+        final ConfigData result = instance.get("", Collections.singleton("DATABASE_HOSTNAME"));
+        assertEquals("a.test.db", result.data().get("DATABASE_HOSTNAME"));
+    }
+
+    @Test
+    public void testExceptionWhenMissingEnvironmentVariable() {
+        assertThrows(EnvironmentVariableMissingException.class, () -> instance.get("", Collections.singleton("key")));
+    }
+
+    @Test
+    public void testExceptionThrownOnPathPlusKey() {
+        assertThrows(IllegalArgumentException.class, () -> instance.get("path", Collections.singleton("key")));
+    }
+}
